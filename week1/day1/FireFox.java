34049c0834cf02f723ae9ddbc81eeb2fda9b8a32
Pavithra-171 <147306717+Pavithra-171@users.noreply.github.com>
2023-10-08 09:50:44 +0100

    Add files via upload
    Day1

---
+++ week1/day1/FireFox.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class FireFox {
+public static void main(String[] args) {
+	float browserVersions=100.2f;
+	String browserName="firefox";
+	boolean isVisible=true;
+	int releaseYear=1998;
+	char browserLogo='f';
+	System.out.println(browserVersions);
+	System.out.println(browserName);
+	System.out.println(isVisible);
+	System.out.println(releaseYear);
+	System.out.println(browserLogo);
+	
+	
+}
+}
